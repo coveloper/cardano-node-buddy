b56ea5ff6fc6119878f39bf3869159807dcbde97
Jon Bauer <jon@covelop.org>
2022-12-03 21:29:51 -0800

    Initial Mac Project state

---
+++ CardanoNodeBuddy/CardanoNodeBuddy/CardanoNodeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardanoNodeBuddyApp.swift
+//  CardanoNodeBuddy
+//
+//  Created by Jon Bauer on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CardanoNodeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
