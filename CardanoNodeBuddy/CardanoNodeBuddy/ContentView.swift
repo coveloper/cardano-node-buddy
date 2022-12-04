c839227ef997fdac25874fa5c56869ed6a7bdf4a
Jon Bauer <jon@covelop.org>
2022-12-03 22:06:27 -0800

    say text from input

--- CardanoNodeBuddy/CardanoNodeBuddy/ContentView.swift
+++ CardanoNodeBuddy/CardanoNodeBuddy/ContentView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = "Hello, World!"
+    @State var isRunning = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("SayThis")
+                .font(.largeTitle)
+                .padding()
+            HStack {
+                TextField("Message", text: $message)
+                    .padding(.leading)
+                Button(action: {
+                    let executableURL = URL(fileURLWithPath: "/usr/bin/say")
+                    self.isRunning = true
+                    try! Process.run(executableURL,
+                                     arguments: [self.message],
+                                     terminationHandler: { _ in self.isRunning = false })
+                }) {
+                    Text("Say")
+                }.disabled(isRunning)
+                    .padding(.trailing)
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
